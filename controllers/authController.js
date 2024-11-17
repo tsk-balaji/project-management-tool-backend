090e06bdfb8690579bdec7e7bc10cb1b063801e0
tsk-balaji <tskbalaji15@gmail.com>
2024-11-18 03:46:04 +0530

    getAllUsers

--- controllers/authController.js
+++ controllers/authController.js
@@ -262,3 +262,15 @@ exports.resetPassword = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+//Get All Users
+exports.getAllUsers = async (req, res) => {
+  try {
+    // Fetch all users from the database
+    const users = await User.find().select("-password"); // Exclude password field for security
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Error fetching users" });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const {
   activateAccount,
   forgotPassword,
   resetPassword,
+  getAllUsers,
 } = require("../controllers/authController");
 
 const router = express.Router();
@@ -15,5 +16,6 @@ router.post("/login", loginUser);
 router.get("/activate/:token", activateAccount);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
+router.get("/users", getAllUsers);
 
 module.exports = router;
